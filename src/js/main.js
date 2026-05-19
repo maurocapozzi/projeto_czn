54d1f0ca9d9ceb6b27ce549300c257f1672c8eb0
mauro capozzi <maurocapozzi@yahoo.com.br>
2026-05-19 10:15:46 -0300

    Initial commit: Boilerplate HTML, CSS, JS e Docker

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Arquivo principal de JavaScript
+ */
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('CZN Project Inicializado com Sucesso!');
+
+    // O código da aplicação deve ser organizado e instanciado aqui
+    // Exemplo:
+    // const menu = new MobileMenu();
+    // menu.init();
+});
